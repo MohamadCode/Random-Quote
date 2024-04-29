9fe5d320fac2a47895695e338ad5987e35835af1
MohamadCode <m-munla2004@hotmail.com>
2024-04-29 10:10:21 +0300

    Quotes

--- App.js
+++ App.js
@@ -1,2 +1,3 @@
 let Quotediv = document.querySelector(".quote-container")
-console.log(Quotediv)
+let Quotes = ["Do the Hard work, especially when you don't feel like it!'Hamza Ahmed' ", "your new life is gonna cost you your old one ! 'Profound Learning' ", "To Become who you want,  you must sacrfice who you are ! 'Zayn' ", "STAY HARD 'David Goggins'" ]
+console.log(Quotes)
