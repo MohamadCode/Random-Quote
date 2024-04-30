7aea39ebbe08c35251ee6e1c22a00f27af212586
MohamadCode <m-munla2004@hotmail.com>
2024-04-30 10:14:32 +0300

    Quotes function-done!

--- App.js
+++ App.js
@@ -1,3 +1,12 @@
-let Quotediv = document.querySelector(".quote-container")
-let Quotes = ["Do the Hard work, especially when you don't feel like it!'Hamza Ahmed' ", "your new life is gonna cost you your old one ! 'Profound Learning' ", "To Become who you want,  you must sacrfice who you are ! 'Zayn' ", "STAY HARD 'David Goggins'" ]
-console.log(Quotes)
+const Quotediv = document.getElementById("quoteid")
+const Quotes = ["Do the Hard work, especially when you don't feel like it!'Hamza Ahmed' ",
+"your new life is gonna cost you your old one ! 'Profound Learning' ", 
+"To Become who you want,  you must sacrfice who you are ! 'Zayn' ", 
+"STAY HARD 'David Goggins'" ]
+
+
+genrate_quote.addEventListener("click", function() { 
+    Quotediv.textContent = Quotes [Math.floor(Math.random() * Quotes.length)]
+    
+
+})
